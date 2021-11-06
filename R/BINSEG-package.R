a1dbbe30a52c0538b94687884908c9bf3c5b190c
Shadmaan Hye <sh2742@nau.edu>
2021-11-06 11:53:50 -0700

    Added the project

---
+++ R/BINSEG-package.R
@@ -0,0 +1,15 @@
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib BINSEG, .registration = TRUE
+## usethis namespace: end
+NULL
+
+#' BINSEG: A package for applying Binary Segmentation algorithm
+#'
+#' The Binseg package provides the following function:
+#' apply_binseg
+#'
+#' @docType package
+#' @name BINSEG
+NULL
+#> NULL
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(BINSEG)
+
+test_check("BINSEG")
